d75f5794b9b77b2eb3140d5bb0a6ab04f3431b2c
Matthew Mueller <mattmuelle@gmail.com>
2015-07-24 13:59:28 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,58 @@
+
+# oauth-popup
+
+  minimal popup for authenticating through oauth
+
+## Installation
+
+```
+npm install oauth-popup
+```
+
+## Usage
+
+```js
+popup('https://www.facebook.com/v2.3/dialog/oauth?...', function(err, code) {
+  if (err) throw err;
+  console.log(code)
+});
+```
+
+## API
+
+### `popup(url, options, fn)`
+
+Open a popup and go to the following `url`, calling `fn` when the flow is complete.
+
+Available `options` include:
+
+- name: name of the window
+- width: width of the popup
+- height: height of the popup
+- left: position of the popup
+- top: position of the popup
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2015 Matthew Mueller &lt;mattmuelle@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,94 @@
+/**
+ * Module dependencies
+ */
+
+var querystring = require('querystring');
+var assign = require('object-assign');
+var once = require('once');
+
+/**
+ * Export `popup`
+ */
+
+module.exports = popup;
+
+/**
+ * Initialize `popup`
+ */
+
+function popup(url, options, fn) {
+  options = options || {};
+
+  if (2 == arguments.length) {
+    fn = options;
+    options = {};
+  }
+
+  var str = stringify(configure(options));
+  var popup = window.open(url, options.name || '', str);
+  popup.focus();
+  poll(popup, fn);
+  return popup;
+}
+
+/**
+ * Poll
+ */
+
+function poll(popup, fn) {
+  var done = once(fn);
+
+  setInterval(function polling() {
+    try {
+      var documentOrigin = document.location.host;
+      var popupWindowOrigin = popup.location.host;
+    } catch (e) {};
+
+    if (popupWindowOrigin === documentOrigin && (popup.location.search || popup.location.hash)) {
+      var queryParams = popup.location.search.substring(1).replace(/\/$/, '');
+      var hashParams = popup.location.hash.substring(1).replace(/[\/$]/, '');
+      var hash = querystring.parse(hashParams);
+      var qs = querystring.parse(queryParams);
+
+      qs = assign(qs, hash);
+
+      if (qs.error) {
+        clearInterval(polling);
+        popup.close();
+        done(new Error(qs.error));
+      } else {
+        clearInterval(polling);
+        popup.close();
+        done(null, qs);
+      }
+    }
+
+  }, 35);
+}
+
+/**
+ * Configure the popup
+ */
+
+function configure(options) {
+  var width = options.width || 500;
+  var height = options.height || 500;
+  return assign({
+    width: width,
+    height: height,
+    left: window.screenX + ((window.outerWidth - width) / 2),
+    top: window.screenY + ((window.outerHeight - height) / 2.5)
+  }, options || {});
+}
+
+/**
+ * Stringify
+ */
+
+function stringify(obj) {
+  var parts = [];
+  for (var key in obj) {
+    parts.push(key + '=' + obj[key]);
+  }
+  return parts.join(',');
+}
